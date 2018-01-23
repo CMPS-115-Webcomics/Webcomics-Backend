33057326c4fbb2a5deedafd5e1a873745753d6b9
William Ritson <william.ritson@gmail.com>
2018-01-22 18:29:14 -0800

    Added database connection code.
    Added password hashing.
    
    Started building authentication routes.

--- sql/makeDB.sql
+++ sql/makeDB.sql
@@ -1,46 +1,56 @@
-CREATE TABLE Author (
-    authorID            INTEGER PRIMARY KEY,
-    name                VARCHAR(30) UNIQUE,
-    url                 VARCHAR(30) UNIQUE,
-    biography           TEXT
+DROP SCHEMA Comics CASCADE; 
+CREATE SCHEMA Comics;
+
+CREATE TABLE Comics.Account (
+    accountID           SERIAL PRIMARY KEY,
+    username            VARCHAR(30) UNIQUE,
+    profileURL          VARCHAR(30) UNIQUE,
+    email               VARCHAR(254) UNIQUE,
+    emailToken          VARCHAR(32),
+    emailVerified       BOOLEAN DEFAULT false,
+    biography           TEXT,
+    password            VARCHAR(256),
+    salt                VARCHAR(32)
 );
 
-CREATE TABLE Comic (
-    comicID             INTEGER PRIMARY KEY,
-    authorID            INTEGER NOT NULL,
-    name                VARCHAR(50),
-    url                 VARCHAR(30) UNIQUE,
+CREATE TABLE Comics.Comic (
+    comicID             SERIAL PRIMARY KEY,
+    accountID           INTEGER NOT NULL,
+    title               VARCHAR(50) UNIQUE,
+    comicURL            VARCHAR(30) UNIQUE,
     description         VARCHAR(500),
-    FOREIGN KEY (authorID) REFERENCES Author(authorID)
+    FOREIGN KEY (accountID) REFERENCES Comics.Account(accountID)
 );
 
-CREATE TABLE Volume (
-    volumeID            INTEGER,
-    comicID             INTEGER,
-    chapterNumber       INTEGER UNIQUE,
-    author              INTEGER,
-    name                VARCHAR(50) UNIQUE,
-    PRIMARY KEY (volumeID),
-    FOREIGN KEY (comicID) REFERENCES Comic(comicID)
-);
-
-CREATE TABLE Chapter (
-    chapterID           INTEGER,
-    volumeID            INTEGER,
-    chapterNumber       INTEGER UNIQUE,
-    author              INTEGER,
-    name                VARCHAR(50) UNIQUE,
-    PRIMARY KEY (chapterID),
-    FOREIGN KEY (comicID) REFERENCES Comic(comicID)
-);
-
-CREATE TABLE Page (
-    pageNumber          INTEGER,
-    chapterNumber       INTEGER,
-    volumeNumber        INTEGER
+CREATE TABLE Comics.Volume (
+    volumeID            SERIAL PRIMARY KEY,
     comicID             INTEGER NOT NULL,
+    volumeNumber        INTEGER,
+    name                VARCHAR(50) UNIQUE,
+    UNIQUE( volumeNumber, comicID )
+    FOREIGN KEY (comicID) REFERENCES Comics.Comic(comicID)
+);
+
+CREATE TABLE Comics.Chapter (
+    chapterID           SERIAL PRIMARY KEY,
+    volumeID            INTEGER,
+    chapterNumber       SERIAL,
+    name                VARCHAR(50) UNIQUE,
+    comicID             INTEGER,
+    UNIQUE( chapterNumber, volumeID, comicID )
+    FOREIGN KEY (volumeID) REFERENCES Comics.Volume(volumeID),
+    FOREIGN KEY (comicID) REFERENCES Comics.Comic(comicID)
+);
+
+CREATE TABLE Comics.Page (
+    pageId              SERIAL PRIMARY KEY,
+    pageNumber          SERIAL,
+    chapterID           INTEGER,
+    comicID             INTEGER,
     author              INTEGER,
     altText             VARCHAR(300),
-    PRIMARY KEY (pageNumber, chapterNumber, volumeNumber),
-    FOREIGN KEY (comicID) REFERENCES Comic(comicID)
+    imgURL              VARCHAR(128),
+    UNIQUE ( pageNumber, comicID, chapterID ),
+    FOREIGN KEY (chapterID) REFERENCES Comics.chapterID(chapterID),
+    FOREIGN KEY (comicID) REFERENCES Comics.Comic(comicID)
 );
