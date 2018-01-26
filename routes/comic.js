bc0223801913590a0471c19ccc5b8d941eccb2b8
sajia <sajia@ucsc.edu>
2018-01-26 12:59:06 -0800

    changed routes/comic.js by adding POST that returns what it gets - will actually store data at a later date

--- routes/comic.js
+++ routes/comic.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(4200);
+
 var router = express.Router();
 
 /* Get a list of all comics. */
 router.get('/', function(req, res, next) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    
 });
 
 /* Get a single comic */
@@ -13,7 +21,18 @@ router.get('/:id', function(req, res, next) {
 
 /* Generate a new comic */
 router.post('/', function(req, res, next) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    /*var comic = new Comic();
+    comic.name = req.body.name;
+    comic.desc = req.body.desc;
+
+    comic.save(function(err){
+        if (err){
+            res.send(err);
+        }
+    })
+    */
+    res.json({ message: req.body}); 
 });
 
+app.use('/api', router);
 module.exports = router;
