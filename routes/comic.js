acadcd59ae7b05897478e6a803587f7b6c71c526
Yinghao Wang <yinghao.a.wang@gmail.com>
2018-01-29 04:59:20 -0800

    added/changed fields to work with frontend

--- routes/comic.js
+++ routes/comic.js
@@ -30,7 +30,7 @@ router.get('/list', function(req, res, next) {
             });
         },
         function(client, release, callback) {
-            client.query(`SELECT title, comicURL, description, thumbnailURL 
+            client.query(`SELECT comicID, accountID, title, comicURL, description, thumbnailURL 
                           FROM Comics.Comic ORDER BY title`, (err, result) => {
             //client.query('SELECT * FROM comics.account', (err, result) => {
                 if (err) {
@@ -54,8 +54,8 @@ router.get('/list', function(req, res, next) {
     });
 });
 
-/* Get a single comic */
-router.get('/:id', function(req, res, next) {
+/* Get a single comic from url */
+router.get('/comicURL/:comicURL', function(req, res, next) {
     async.waterfall([
         function(callback) {
             pool.connect((err, client, release) => {
@@ -68,25 +68,66 @@ router.get('/:id', function(req, res, next) {
             });
         },
         function(client, release, callback) {
-            client.query(`SELECT chapterNumber, name
-                          FROM Comics.Chapter
-                          WHERE comicID = $1`, [req.params.id], (err, result) => {
+            client.query('SELECT * FROM Comics.Comic WHERE comicURL = $1',[req.params.comicURL], (err, result) => {
                 if (err) {
                     release();
                     console.error('Error executing query', err.stack);
                     callback(err);
                 } else {
-                    callback(null, result);
+                    var data = result.rows[0];
+                    callback(null, client, release, data);
                 }
             });
-        }
+        },
+        function(client, release, data, callback) {
+            client.query(`SELECT *
+                          FROM Comics.Chapter
+                          WHERE comicID = $1`, [req.params.id], (err, result) => {
+                if (err) {
+                    release();
+                    console.error(err.stack);
+                    callback(err);
+                } else {
+                    data['chapters'] = result.rows;
+                    callback(null, client, release, data);
+                }
+            });
+        },
+        function(client, release, data, callback) {
+            client.query(`SELECT *
+                          FROM Comics.Volume
+                          WHERE comicID = $1`, [req.params.id], (err, result) => {
+                if (err) {
+                    release();
+                    console.error(err.stack);
+                    callback(err);
+                } else {
+                    data['volumes'] = result.rows;
+                    callback(null, client, release, data);
+                }
+            });
+        },
+        function(client, release, data, callback) {
+            client.query(`SELECT *
+                          FROM Comics.Page
+                          WHERE comicID = $1`, [req.params.id], (err, result) => {
+                if (err) {
+                    release();
+                    console.error(err.stack);
+                    callback(err);
+                } else {
+                    release();
+                    data['pages'] = result.rows;
+                    callback(null, data);
+                }
+            });
+        },
     ],
     function(err, results) {
         if (err) {
             res.send(err);
         } else {
-            res.send(results.rows);
-            //res.status(200).json({data: result});
+            res.status(200).send(results);
         }
     });
 });
@@ -138,12 +179,165 @@ router.post('/create', function(req, res, next) {
             });
         },
         function(client, release, callback) {
-            console.log(req.body);
             client.query(`INSERT INTO Comics.comic 
                           (accountID, title, comicURL, thumbnailURL, description)
                           VALUES ($1, $2, $3, $4, $5)`, [
                //req.user, //actual one to use
-                req.body.user, //testing only
+                req.body.userID, //testing only
+                req.body.title,
+                req.body.comicURL,
+                req.body.thumbnailURL,
+                req.body.description
+             ], (err, result) => {
+                release();
+                if (err) {
+                    console.error('Query cannot be executed', err.stack);
+                    callback(err);
+                } else {
+                    callback(null, result);
+                }
+             });
+        }
+    ],
+    function(err, results) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(results);
+        }
+    });
+});
+
+/* Get a single comic */
+router.get('/id/:id', function(req, res, next) {
+    async.waterfall([
+        function(callback) {
+            pool.connect((err, client, release) => {
+                if (err) {
+                    console.error('Error acquiring client', err.stack);
+                    callback(err);
+                } else {
+                    callback(null, client, release);
+                }
+            });
+        },
+        function(client, release, callback) {
+            client.query('SELECT * FROM Comics.Comic WHERE comicID = $1',[req.params.id], (err, result) => {
+                if (err) {
+                    release();
+                    console.error('Error executing query', err.stack);
+                    callback(err);
+                } else {
+                    var data = result.rows[0];
+                    callback(null, client, release, data);
+                }
+            });
+        },
+        function(client, release, data, callback) {
+            client.query(`SELECT *
+                          FROM Comics.Chapter
+                          WHERE comicID = $1`, [req.params.id], (err, result) => {
+                if (err) {
+                    release();
+                    console.error(err.stack);
+                    callback(err);
+                } else {
+                    data['chapters'] = result.rows;
+                    callback(null, client, release, data);
+                }
+            });
+        },
+        function(client, release, data, callback) {
+            client.query(`SELECT *
+                          FROM Comics.Volume
+                          WHERE comicID = $1`, [req.params.id], (err, result) => {
+                if (err) {
+                    release();
+                    console.error(err.stack);
+                    callback(err);
+                } else {
+                    data['volumes'] = result.rows;
+                    callback(null, client, release, data);
+                }
+            });
+        },
+        function(client, release, data, callback) {
+            client.query(`SELECT *
+                          FROM Comics.Page
+                          WHERE comicID = $1`, [req.params.id], (err, result) => {
+                if (err) {
+                    release();
+                    console.error(err.stack);
+                    callback(err);
+                } else {
+                    release();
+                    data['pages'] = result.rows;
+                    callback(null, data);
+                }
+            });
+        },
+    ],
+    function(err, results) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.status(200).send(results);
+        }
+    });
+});
+
+//Generate a new comic 
+router.post('/create', function(req, res, next) {
+    async.waterfall([
+        function(callback) {
+            pool.connect((err, client, release) => {
+                if (err) {
+                    console.error('Error acquiring client', err.stack);
+                    callback(err);
+                } else {
+                    callback(null, client, release);
+                }
+            });
+        },
+        function(client, release, callback) {
+            client.query(`SELECT title FROM Comics.Comic WHERE title=$1`, [req.body.title], (err, result) => {
+                if (err) {
+                    release();
+                    console.error('Query for title failed', err.stack);
+                    callback(err);
+                } else if (result.rowCount > 0){
+                    release();
+                    var msg = 'Title is already taken';
+                    console.error(msg);
+                    callback(msg);
+                } else {
+                    callback(null, client, release);
+                }
+            });
+        },
+        function(client, release, callback) {
+            client.query(`SELECT comicURL FROM Comics.Comic
+                          WHERE comicURL=$1`, [req.body.comicURL], (err, result) => {
+                if (err) {
+                    release();
+                    console.error('Query for comicURL failed', err.stack);
+                    callback(err)
+                } else if (result.rowCount > 0){
+                    release();
+                    var msg = 'Comic URL is already taken';
+                    console.error(msg);
+                    callback(msg);
+                } else {
+                    callback(null, client, release);
+                }
+            });
+        },
+        function(client, release, callback) {
+            client.query(`INSERT INTO Comics.comic 
+                          (accountID, title, comicURL, thumbnailURL, description)
+                          VALUES ($1, $2, $3, $4, $5)`, 
+            [
+                req.body.userID, //testing only
                 req.body.title,
                 req.body.comicURL,
                 req.body.thumbnailURL,
