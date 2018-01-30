db9fa7f5463f6327685a69fe8b5fbb0f48dcd661
William Ritson <william.ritson@gmail.com>
2018-01-29 22:54:22 -0800

    Fixed CORS

--- app.js
+++ app.js
@@ -28,22 +28,12 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
-  next();
-});
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
-
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
 
 app.use('/', index);
 app.use('/api/users', users);
@@ -66,8 +56,8 @@ app.use(function (err, req, res, next) {
   console.error(err);
 
   // render the error page
-  res.status(err.status || 500);
-  res.send(err);
+  res.status(err.status || 500)
+     .send(err);
 });
 
 module.exports = app;
