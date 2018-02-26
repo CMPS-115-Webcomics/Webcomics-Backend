a71c3611896c252a59afb0a9fae7944fe63ffc2c
William Ritson <william.ritson@gmail.com>
2018-02-26 15:30:43 -0800

    Checks of email/username are now case insensative

--- routes/availability.js
+++ routes/availability.js
@@ -2,7 +2,9 @@
 
 const express = require('express');
 const router = express.Router();
-const { db } = require('../db');
+const {
+    db
+} = require('../db');
 
 /**
  * Checks if a certain table has a row with the given attribute.
@@ -11,12 +13,16 @@ const { db } = require('../db');
  *
  * @param {string} table The table whose attribute we are checking
  * @param {string} attribute The key of the table
+ * @param {boolean} caseInsensitive Whether the search should ignore capitiization
  * @returns {function(Request, Response, NextFunction): void} Route handler that checks the attribute
  */
-const checkAvalibility = (table, attribute) => {
+const checkAvalibility = (table, attribute, caseInsensitive) => {
     return async (req, res, next) => {
         try {
-            const query = await db.query(`SELECT ${attribute} FROM Comics.${table} WHERE ${attribute}=$1`, [req.params[attribute]]);
+            const sql = caseInsensitive ?
+                `SELECT ${attribute} FROM Comics.${table} WHERE LOWER(${attribute})=LOWER($1)` :
+                `SELECT ${attribute} FROM Comics.${table} WHERE ${attribute}=$1`;
+            const query = await db.query(sql, [req.params[attribute]]);
             res.json({
                 available: query.rowCount === 0
             });
@@ -45,8 +51,8 @@ router.get('/emailorpassword/:nameOrPass', async (req, res, next) => {
 });
 
 
-router.get('/email/:email', checkAvalibility('Account', 'email'));
-router.get('/username/:username', checkAvalibility('Account', 'username'));
+router.get('/email/:email', checkAvalibility('Account', 'email', true));
+router.get('/username/:username', checkAvalibility('Account', 'username', true));
 router.get('/title/:title', checkAvalibility('Comic', 'title'));
 router.get('/comicURL/:comicURL', checkAvalibility('Comic', 'comicURL'));
 
