1d42c4dfaf6a5f545eb1253dbf4101e396741f96
William Ritson <william.ritson@gmail.com>
2018-03-11 19:36:14 -0700

    Added routes for weekly scheduling

--- routes/schedule.js
+++ routes/schedule.js
@@ -9,18 +9,44 @@ const validators = require('./validators');
 const tokens = require('../tokens');
 
 // allows a user to view their personal release schedule
-router.get('/mySchedule', tokens.authorize, async (req, res, next) => {
+router.get('/getSchedule/:comicID', async (req, res, next) => {
     try {
         const schedQuery = await db.query(`
             SELECT s.comicID, s.updateDay, s.updateType, s.updateWeek
             FROM Comics.Schedule s
-            WHERE s.comicID = (SELECT c.comicID FROM Comics.comic c WHERE c.AccountID = $1)`, [req.user.accountID]);
+            WHERE s.comicID = $1`, [req.params.comicID]);
         res.json(schedQuery.rows);
     } catch (err) {
         next(err);
         return;
     }
 });
+
+// allows a user to set a weekly schedule
+router.put('/setWeeklySchedule',
+    tokens.authorize,
+    validators.requiredAttributes(['comicID', 'updateDays']),
+    validators.canModifyComic,
+    async (req, res, next) => {
+        try {
+            await db.query(`
+                DELETE FROM Comics.Schedule
+                WHERE comicID = $1`, [req.body.comicID]);
+            const insertions = Array.from(req.body.updateDays, day => {
+                return db.query(`
+                    INSERT INTO Comics.Schedule (comicID, updateDay, updateType)
+                    VALUES($1, $2, 'weekly')`, [req.body.comicID, day]);
+            });
+            await Promise.all(insertions);
+            res.status(200).json({
+                message: 'ok'
+            });
+        } catch (err) {
+            next(err);
+            return;
+        }
+    }
+);
 
 // allows a user to edit their own release schedule
 router.put('/editSchedule',
@@ -46,4 +72,3 @@ router.put('/editSchedule',
 );
 
 module.exports = router;
-
